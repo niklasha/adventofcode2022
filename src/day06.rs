4af50237d6f7431e981ffdbaa99538ef4dabda99
Niklas Hallqvist <niklas@appli.se>
2022-12-06 06:58:44 +0100

    First solution to day 6, slurps all of the input, should be redone into a streaming solution.

---
+++ src/day06.rs
@@ -0,0 +1,85 @@
+use crate::day::*;
+use std::collections::HashSet;
+use std::hash::Hash;
+use std::io::Read;
+
+pub struct Day06 {}
+
+type Output = usize;
+
+impl Day for Day06 {
+    fn tag(&self) -> &str {
+        "06"
+    }
+
+    fn part1(&self, input: &dyn Fn() -> Box<dyn io::Read>) {
+        println!("{:?}", self.part1_impl(&mut *input()));
+    }
+
+    fn part2(&self, input: &dyn Fn() -> Box<dyn io::Read>) {
+        println!("{:?}", self.part2_impl(&mut *input()));
+    }
+}
+
+fn has_unique_elements<T>(iter: T) -> bool
+where
+    T: IntoIterator,
+    T::Item: Eq + Hash,
+{
+    let mut uniq = HashSet::new();
+    iter.into_iter().all(move |x| uniq.insert(x))
+}
+
+impl Day06 {
+    fn scan(input: &mut dyn io::Read, size: usize) -> BoxResult<Output> {
+        let input = io::BufReader::new(input)
+            .bytes()
+            .collect::<Result<Vec<_>, _>>()?;
+        let (i, _) = input
+            .as_slice()
+            .windows(size)
+            .enumerate()
+            .find(|(_, w)| has_unique_elements(w.iter()))
+            .ok_or(AocError)?;
+        Ok(i + size)
+    }
+
+    fn part1_impl(&self, input: &mut dyn io::Read) -> BoxResult<Output> {
+        Self::scan(input, 4)
+    }
+
+    fn part2_impl(&self, input: &mut dyn io::Read) -> BoxResult<Output> {
+        Self::scan(input, 14)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test1(s: &str, f: Output) {
+        assert_eq!(Day06 {}.part1_impl(&mut s.as_bytes()).ok(), Some(f));
+    }
+
+    #[test]
+    fn part1() {
+        test1("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7);
+        test1("bvwbjplbgvbhsrlpgdmjqwftvncz", 5);
+        test1("nppdvjthqldpwncqszvftbrmjlhg", 6);
+        test1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10);
+        test1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11);
+    }
+
+    fn test2(s: &str, f: Output) {
+        assert_eq!(Day06 {}.part2_impl(&mut s.as_bytes()).ok(), Some(f));
+    }
+
+    #[test]
+    fn part2() {
+        test2("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19);
+        test2("bvwbjplbgvbhsrlpgdmjqwftvncz", 23);
+        test2("nppdvjthqldpwncqszvftbrmjlhg", 23);
+        test2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29);
+        test2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26);
+    }
+}
