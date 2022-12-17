d03e46e0a26985573ccd7c272563e728b3cbb3e2
Niklas Hallqvist <niklas@appli.se>
2022-12-17 17:45:50 +0100

    Day 16 part 1 solution, the algorithm was correct already yesterday, but a bitmask datatype was too narrow, and I had chosen to circumvent Rust's helping type system, by using ordinary integers and release builds, which skip overflow tests on shift operations.  Lesson learned!

--- src/day16.rs
+++ src/day16.rs
@@ -100,8 +100,158 @@ impl Hash for State {
     }
 }
 
+struct Cave {
+    name_to_valve: HashMap<String, u8>,
+    openable: Vec<u8>,
+    neighbours: Vec<u64>,
+    rate: Vec<Output>,
+}
+
+const OPENABLE_COUNT: usize = 16;
+const NOT_OPENABLE: u8 = (OPENABLE_COUNT - 1) as u8;
+
+impl Cave {
+    fn parse(input: &mut dyn Read) -> BoxResult<Self> {
+        let valves: Vec<Valve> = io::BufReader::new(input)
+            .lines()
+            .map(|l| l.map_err(|e| e.into()))
+            .map(|l| l.and_then(|l| l.parse::<Valve>()))
+            .collect::<BoxResult<_>>()?;
+        assert!(valves.len() <= u8::MAX as usize + 1);
+        let name_to_valve: HashMap<String, u8> = valves
+            .iter()
+            .enumerate()
+            .map(|(i, _)| (valves[i].name.to_owned(), i as u8))
+            .collect();
+        assert!(valves.iter().filter(|v| v.rate > 0).count() <= NOT_OPENABLE as usize);
+        let mut openable = vec![NOT_OPENABLE; valves.len()];
+        for (j, (i, v)) in valves
+            .iter()
+            .enumerate()
+            .filter(|(_, v)| v.rate > 0)
+            .enumerate()
+        {
+            openable[i] = j as u8;
+        }
+        let neighbours = valves
+            .iter()
+            .map(|v| {
+                v.neighbours.iter().fold(Ok(0), |set, neighbour| {
+                    set.and_then(
+                        |set| Ok(set | 1 << *name_to_valve.get(neighbour).ok_or(AocError)?),
+                    )
+                })
+            })
+            .collect::<BoxResult<_>>()?;
+        let rate = valves
+            .iter()
+            .filter(|v| v.rate > 0)
+            .map(|v| v.rate)
+            .collect::<Vec<_>>();
+        println!("{:?}", name_to_valve);
+        println!("{:?}", openable);
+        println!("{:?}", neighbours);
+        println!("{:?}", rate);
+        Ok(Cave {
+            name_to_valve,
+            openable,
+            neighbours,
+            rate,
+        })
+    }
+
+    fn traverse(
+        &self,
+        valve_no: u8,
+        valves_state: u16,
+        visited: &mut [u8],
+        t: usize,
+        eol: usize,
+        flow: usize,
+        flow_max: &mut usize,
+    ) {
+        if t > eol {
+            if flow > *flow_max {
+                *flow_max = flow;
+                //                println!("NEW MAX {}", flow);
+            }
+            return;
+        }
+        let index = (valve_no as usize) << OPENABLE_COUNT | valves_state as usize;
+        let offset = index / 8;
+        let visit_mask = 1 << (index % 8) as u16;
+        if (visited[offset] & visit_mask) == 0 {
+            visited[offset] |= visit_mask;
+            let delta = (0..NOT_OPENABLE)
+                .filter(|valve_no| valves_state & (1 << valve_no) != 0)
+                .map(|valve_no| self.rate[valve_no as usize])
+                .sum::<usize>();
+            // println!(
+            //     "t {} valve {} states {} flow {} delta {}",
+            //     t, valve_no, valves_state, flow, delta
+            // );
+            let flow = flow + delta;
+            let mut did_walk = false;
+            let openable_valve_no = self.openable[valve_no as usize] as u16;
+            if openable_valve_no != NOT_OPENABLE as u16 {
+                let mask = 1 << openable_valve_no;
+                let bit = valves_state & mask;
+                if bit == 0 {
+                    self.traverse(
+                        valve_no,
+                        valves_state | mask,
+                        visited,
+                        t + 1,
+                        eol,
+                        flow,
+                        flow_max,
+                    );
+                    did_walk = true;
+                }
+            }
+            let mut neighbours = self.neighbours[valve_no as usize];
+            let mut neighbour = 0u8;
+            while neighbours != 0 {
+                let ffs = neighbours.trailing_zeros() as u8;
+                neighbour += ffs;
+                self.traverse(neighbour, valves_state, visited, t + 1, eol, flow, flow_max);
+                did_walk = true;
+                neighbours >>= ffs + 1;
+                neighbour += 1;
+            }
+            visited[offset] &= !visit_mask;
+            //     if !did_walk {
+            //         self.traverse(valve_no, valves_state, visited, t + 1, eol, flow, flow_max);
+            //     }
+        }
+    }
+}
+
+struct VisitState(u32);
+
+impl VisitState {
+    fn from(valve: u8, visited: u16) -> Self {
+        VisitState((valve as u32) << OPENABLE_COUNT | visited as u32)
+    }
+}
+
 impl Day16 {
     fn process(input: &mut dyn io::Read, n: usize) -> BoxResult<Output> {
+        let cave = Cave::parse(input)?;
+        let mut flow_max = 0;
+        cave.traverse(
+            *cave.name_to_valve.get("AA").ok_or(AocError)?,
+            0,
+            &mut [0; 64 * (u16::MAX as usize + 1) / 8],
+            1,
+            n,
+            0,
+            &mut flow_max,
+        );
+        Ok(flow_max)
+    }
+
+    fn process_old(input: &mut dyn io::Read, n: usize) -> BoxResult<Output> {
         let cave = Self::parse(input)?;
         let init = vec![(
             State {
@@ -239,7 +389,7 @@ Valve JJ has flow rate=21; tunnel leads to valve II",
         assert_eq!(Day16 {}.part2_impl(&mut s.as_bytes(), n).ok(), Some(f));
     }
 
-    //#[test]
+    #[test]
     fn part2() {
         test2(
             "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -252,8 +402,8 @@ Valve GG has flow rate=0; tunnels lead to valves FF, HH
 Valve HH has flow rate=22; tunnel leads to valve GG
 Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II",
-            30,
-            45000,
+            26,
+            1707,
         );
     }
 }
